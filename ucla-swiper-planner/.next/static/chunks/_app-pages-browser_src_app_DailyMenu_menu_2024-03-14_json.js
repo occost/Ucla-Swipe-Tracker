7a0e6ea8149396b15be6cad9cbe3b74aaf300288
EideenMoz <eideenm11@gmail.com>
2024-03-11 17:17:49 -0700

    Ignore This Commit, Messing With Deployment
    I'm pushing these files to the remote for now, but
    we are going to fix deployment soon.

---
+++ ucla-swiper-planner/.next/static/chunks/_app-pages-browser_src_app_DailyMenu_menu_2024-03-14_json.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["_app-pages-browser_src_app_DailyMenu_menu_2024-03-14_json"],{
+
+/***/ "(app-pages-browser)/./src/app/DailyMenu/menu_2024-03-14.json":
+/*!************************************************!*\
+  !*** ./src/app/DailyMenu/menu_2024-03-14.json ***!
+  \************************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"Breakfast":{"Epicuria":["Closed"],"De Neve":["The Breakfast Hash Brown","Kielbasa","Scrambled Eggs","The Pizzeria Belgian Waffle","The The Griddler"],"Bruin Plate":["Bruin Scramble (ham & Cheddar)","Bruin Scramble (vegetarian)","Egg"," Spinach & Roasted Pepper Sauce On W. W. Bagel","Scrambled Eggs","Vegan Scrambled Eggs","Oat & Whole Wheat Waffle","Whole Wheat Pancakes W/ Amaranth & Chocolate Chips"]},"Lunch":{"Epicuria":["Closed"],"De Neve":["Bar Lemon Garlic Chicken Thighs","The Hearty Vegetable Stew","The Pizzeria Canadian Bacon W/ Caramelized Onion & Spinach Pizza","Pepperoni Deluxe Pizza","The Pastrami Burger","High Carbon Footprint","Montreal Morning"],"Bruin Plate":["Barramundi"," Turnip Greens Whole Wheat Spagheti W/lemon&olive","Lemon Dill Tofu"," Bulgur Wheat"," & Cucumber Salad Bowl","Greek Style Chopped Salad W/ Chicken & Wheat Pita Chips","Spaghetti Squash"," Lentil"," & Cauliflower Taco","Chicken"," Arugula"," & Radish Flatbread","Southwest Black Bean & Avocado Tortilla Pizza","Italian Marinated Chicken Thigh","Farro-mushroom Stacker"]},"Dinner":{"Epicuria":["Chicken Fettuccine Alfredo","Fettuccine Primavera","Armenian Lamb Pizza","\\"roasted Lamb"," Raita"," Za\'atar"," Roasted Pepper"," Mozzarella"," Whole Wheat Flatbread\\"","High Carbon Footprint","Greek Calamari Steak","Frito Mixto De Verdura - Italian Style Fried Vegetables","Roasted Loup De Mer","Crispy Spinach Lentil Cakes W/ Roasted Garlic Aioli","Poulet A La Grecque","Eggplant Steak W/ Chickpeas"," Peppers"," & Feta"],"De Neve":["Bar Seared Gourmet Steak","\\"roasted Beef Steamship\\"","The Front Burner Stupendous Beef Stew","Hearty Vegetable Stew","The Game Over Yoshi","Chicken Drumsticks With Chimichurri Sauce. Portobello Mushroom","The Pizzeria Tmnt Pepperoni Marshmallow Explosion"],"Bruin Plate":["Chicken & Spiced Fregola Bowl","Greek Farro Bowl","Spicy Shrimp","Spicy Red Quinoa & Heirloom Bean Burrito","Garlic Coriander Pork","Pork Tenderloin"," Garlic"," Coriander Seed"," Cumin"," Jalapeno"," Cilantro"," Scallion"," Lime Mole Tofu"]}}');
+
+/***/ })
+
+}]);
---
+++ ucla-swiper-planner/.next/static/chunks/_app-pages-browser_src_app_DailyMenu_menu_2024-03-15_json.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["_app-pages-browser_src_app_DailyMenu_menu_2024-03-15_json"],{
+
+/***/ "(app-pages-browser)/./src/app/DailyMenu/menu_2024-03-15.json":
+/*!************************************************!*\
+  !*** ./src/app/DailyMenu/menu_2024-03-15.json ***!
+  \************************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"Breakfast":{"Epicuria":["Closed"],"De Neve":["The Breakfast Hash Brown","Pork Sausage","Scrambled Eggs","The Pizzeria Belgian Waffle","The Smoked Salmon Egg Benedict"],"Bruin Plate":["Bruin Scramble (ham & Cheddar)","Bruin Scramble (vegetarian)","Chayote"," Tri-tip & Egg Bowl","High Carbon Footprint","Scrambled Eggs","Vegan Scrambled Eggs","Four Grain Waffles","Chunky Apple Oatmeal Pancake"]},"Lunch":{"Epicuria":["Closed"],"De Neve":["Bar Lemon Garlic Chicken Thighs","The Front Burner Macaroni & Cheese","The Country Fried Steak","High Carbon Footprint","Fried Vegan Chick\'n","The Pizzeria Meatball Pizza","High Carbon Footprint","The Patty Melt","Country Gravy Chicken Sandwich"],"Bruin Plate":["Steak"," Black Lentil & Butternut Squash Bowl","High Carbon Footprint","Golden Chickpeas W/ Rice"," Beans & Kale Bowl","Baja Caesar Salad W/ Chipotle Chicken","Vegan Baja Caesar Salad","Cuban Pork Pizza","Tri-color Cauliflower Flatbread","Orange Chipotle Herb Marinated Chicken Thigh"]},"Dinner":{"Epicuria":["Closed"],"De Neve":["Bar Lemon Garlic Chicken Thighs","The Front Burner Chicken Pot Pie","Vegetable Pot Pie","The Blue Plate Special Beef Meatloaf","High Carbon Footprint","Spicy Bbq Tofu Steak","The Pizzeria Meatball Pizza","High Carbon Footprint","The Country Gravy Chicken Sandwich","Patty Melt"],"Bruin Plate":["Pebre-marinated Flank Steak & Guacamole Bowl","High Carbon Footprint","Chayote Squash"," Heirloom Bean Stew"," & Kale Bowl","Garlic Cumin Chicken Wings","Breakfast Taco","Seared Salmon","Southwestern Tofu"]}}');
+
+/***/ })
+
+}]);
