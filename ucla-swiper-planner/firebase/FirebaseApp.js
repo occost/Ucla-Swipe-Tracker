39f7a6678977ae932fbc72eaf2171cc9f80545eb
EideenMoz <eideenm11@gmail.com>
2024-03-13 16:38:12 -0700

    Final Review Changes Prior to Submission
    Reviewed the ReadMe, ran the scraper to get more
    menus. Deleted extraneous files.

--- ucla-swiper-planner/firebase/FirebaseApp.js
+++ ucla-swiper-planner/firebase/FirebaseApp.js
@@ -8,16 +8,6 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: "REDACTED-API-KEY",
-  // authDomain: "REDACTED-AUTH-DOMAIN",
-  // databaseURL: "REDACTED-DATABASE-URL",
-  // projectId: "REDACTED-PROJECT-ID",
-  // storageBucket: "REDACTED-STORAGE-BUCKET",
-  // messagingSenderId: "REDACTED-MESSAGING-SENDER-ID",
-  // appId: "REDACTED-APP-ID",
-  // measurementId: "REDACTED-MEASUREMENT-ID"
-
-
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASEURL,
